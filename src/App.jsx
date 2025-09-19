ba601c8bbae315ac01067e77c4a6d2e65778324e
andercastro <andersteven.castro@gmail.com>
2025-09-19 15:51:25 -0600

    Projects

--- src/App.jsx
+++ src/App.jsx
@@ -8,66 +8,85 @@ import {
   SiJira, SiTrello, SiAndroidstudio, SiTypescript, SiNextdotjs, SiTailwindcss, 
   SiFirebase, SiNginx, SiJest, SiCypress, SiMocha
 } from "react-icons/si";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
 
 export default function Portfolio() {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [index, setIndex] = useState(-1);
+  const [activeProject, setActiveProject] = useState(null);
 
 const techs = [
   // Languages
-  { icon: <FaPython className="text-yellow-500" />, name: "Python", category: "Languages" },
-  { icon: <FaJava className="text-red-600" />, name: "Java", category: "Languages" },
-  { icon: <FaPhp className="text-indigo-600" />, name: "PHP", category: "Languages" },
-  { icon: <FaNodeJs className="text-green-600" />, name: "JavaScript", category: "Languages" },
+  { icon: <FaPython className=" w-10 h-10 text-yellow-500" />, name: "Python", category: "Languages" },
+  { icon: <FaJava className="w-10 h-10 text-red-600" />, name: "Java", category: "Languages" },
+  { icon: <FaPhp className="w-10 h-10 text-indigo-600" />, name: "PHP", category: "Languages" },
+  { icon: <img src="/JavaScript.svg" className="w-10 h-10" />, name: "JavaScript", category: "Languages" },
+  { icon: <img src="/C.svg" className="w-10 h-10" />, name: "C#", category: "Languages" },
+
 
   // Frontend
-  { icon: <FaHtml5 className="text-orange-600" />, name: "HTML5", category: "Frontend" },
-  { icon: <FaCss3Alt className="text-blue-600" />, name: "CSS3", category: "Frontend" },
-  { icon: <SiTailwindcss className="text-sky-400" />, name: "Tailwind CSS", category: "Frontend" },
-  { icon: <SiNextdotjs className="text-gray-900" />, name: "Next.js", category: "Frontend" },
+  { icon: <FaHtml5 className="w-10 h-10 text-orange-600" />, name: "HTML5", category: "Frontend" },
+  { icon: <FaCss3Alt className="w-10 h-10 text-blue-600" />, name: "CSS3", category: "Frontend" },
+  { icon: <SiTailwindcss className="w-10 h-10 text-sky-400" />, name: "Tailwind CSS", category: "Frontend" },
+  { icon: <SiNextdotjs className="w-10 h-10 text-gray-900" />, name: "Next.js", category: "Frontend" },
   { icon: <img src="/AngularJS.svg" className="w-10 h-10" />, name: "Angular.js", category: "Frontend" },
-  { icon: <SiTypescript className="text-blue-600" />, name: "TypeScript", category: "Frontend" },
-  { icon: <FaReact className="text-blue-500" />, name: "React", category: "Frontend" },
-  { icon: <SiVite className="text-yellow-500" />, name: "Vite", category: "Frontend" },
-  { icon: <SiBootstrap className="text-purple-600" />, name: "Bootstrap", category: "Frontend" },
+  { icon: <SiTypescript className="w-10 h-10 text-blue-600" />, name: "TypeScript", category: "Frontend" },
+  { icon: <FaReact className="w-10 h-10 text-blue-500" />, name: "React", category: "Frontend" },
+  { icon: <img src="/Vite.svg" className="w-10 h-10" />, name: "Vite", category: "Frontend" },
+  { icon: <SiBootstrap className="w-10 h-10 text-purple-600" />, name: "Bootstrap", category: "Frontend" },
 
   // Backend
-  { icon: <SiExpress className="text-gray-700" />, name: "Express.js", category: "Backend" },
-  { icon: <SiNestjs className="text-red-600" />, name: "Nest.js", category: "Backend" },
-  { icon: <SiLaravel className="text-red-500" />, name: "Laravel", category: "Backend" },
+  { icon: <FaNodeJs className="w-10 h-10 text-gray-700" />, name: "Node.js", category: "Backend" },
+  { icon: <SiExpress className="w-10 h-10 text-gray-700" />, name: "Express.js", category: "Backend" },
+  { icon: <SiNestjs className="w-10 h-10 text-red-600" />, name: "Nest.js", category: "Backend" },
+  { icon: <SiLaravel className="w-10 h-10 text-red-500" />, name: "Laravel", category: "Backend" },
   { icon: <img src="/Yii.svg" className="w-10 h-10" />, name: "Yii2", category: "Backend" },
   { icon: <img src="/Symfony.svg" className="w-10 h-10" />, name: "Symfony", category: "Backend" },
-  { icon: <FaDocker className="text-blue-500" />, name: "Docker", category: "Backend" },
-  { icon: <SiKubernetes className="text-blue-600" />, name: "Kubernetes", category: "Backend" },
+  { icon: <img src="/Composer.svg" className="w-10 h-10" />, name: "Composer", category: "Backend" },
+  { icon: <img src="/NET.svg" className="w-10 h-10" />, name: "ASP.NET", category: "Backend" },
+  { icon: <img src="/netcore.svg" className="w-10 h-10" />, name: "ASP.NET Core", category: "Backend" },
+  { icon: <FaDocker className="w-10 h-10 text-blue-500" />, name: "Docker", category: "Backend" },
+  { icon: <SiKubernetes className="w-10 h-10 text-blue-600" />, name: "Kubernetes", category: "Backend" },
 
   // Databases
-  { icon: <SiMysql className="text-orange-600" />, name: "MySQL", category: "Databases" },
-  { icon: <SiPostgresql className="text-sky-700" />, name: "PostgreSQL", category: "Databases" },
-  { icon: <SiMongodb className="text-green-700" />, name: "MongoDB", category: "Databases" },
+  { icon: <img src="/MySQL.svg" className="w-10 h-10" />, name: "MySQL", category: "Databases" },
+  { icon: <img src="/SQLServer.svg" className="w-10 h-10" />, name: "Microsoft SQL Server", category: "Databases" },
+  { icon: <SiPostgresql className="w-10 h-10 text-sky-700" />, name: "PostgreSQL", category: "Databases" },
+  { icon: <img src="/DBeaver.svg" className="w-10 h-10" />, name: "DBeaver", category: "Databases" },
+  { icon: <SiMongodb className="w-10 h-10 text-green-700" />, name: "MongoDB", category: "Databases" },
+  { icon: <img src="/Sequelize.svg" className="w-10 h-10" />, name: "Sequelize", category: "Databases" },
 
   // Data Science
   { icon: <img src="/Pandas.svg" className="w-10 h-10" />, name: "Pandas", category: "Data Science" },
   { icon: <img src="/NumPy.svg" className="w-10 h-10" />, name: "NumPy", category: "Data Science" },
 
   // Mobile
-  { icon: <SiKotlin className="text-purple-500" />, name: "Kotlin", category: "Mobile" },
-  { icon: <SiIonic className="text-blue-500" />, name: "Ionic", category: "Mobile" },
-  { icon: <SiAndroidstudio className="text-green-600" />, name: "Android Studio", category: "Mobile" },
+  { icon: <SiKotlin className="w-10 h-10 text-purple-500" />, name: "Kotlin", category: "Mobile" },
+  { icon: <FaJava className="w-10 h-10 text-red-600" />, name: "Java", category: "Mobile" },
+  { icon: <SiIonic className="w-10 h-10 text-blue-500" />, name: "Ionic", category: "Mobile" },
+  { icon: <SiAndroidstudio className="w-10 h-10 text-green-600" />, name: "Android Studio", category: "Mobile" },
 
   // Infrastructure / Cloud
-  { icon: <SiNginx className="text-green-600" />, name: "NGINX", category: "Infrastructure" },
-  { icon: <SiFirebase className="text-yellow-400" />, name: "Firebase", category: "Infrastructure" },
+  { icon: <SiNginx className="w-10 h-10 text-green-600" />, name: "NGINX", category: "Infrastructure" },
+  { icon: <SiFirebase className="w-10 h-10 text-yellow-400" />, name: "Firebase", category: "Infrastructure" },
   { icon: <img src="/AWS.svg" className="w-10 h-10" />, name: "AWS", category: "Infrastructure" },
+  { icon: <img src="/Ubuntu.svg" className="w-10 h-10" />, name: "Ubuntu Server", category: "Infrastructure" },
+
 
   // Testing
-  { icon: <SiJest className="text-red-500" />, name: "Jest", category: "Testing" },
-  { icon: <SiMocha className="text-green-700" />, name: "Mocha", category: "Testing" },
-  { icon: <SiCypress className="text-teal-500" />, name: "Cypress", category: "Testing" },
+  { icon: <SiJest className="w-10 h-10 text-red-500" />, name: "Jest", category: "Testing" },
+  { icon: <SiMocha className="w-10 h-10 text-green-700" />, name: "Mocha", category: "Testing" },
+  { icon: <SiCypress className="w-10 h-10 text-teal-500" />, name: "Cypress", category: "Testing" },
+  { icon: <img src="/Postman.svg" className="w-10 h-10" />, name: "Postman", category: "Testing" },
+
 
   // Tools
   { icon: <img src="/GitHub.svg" className="w-10 h-10" />, name: "Github", category: "Tools" },
-  { icon: <SiJira className="text-blue-400" />, name: "Jira", category: "Tools" },
-  { icon: <SiTrello className="text-sky-400" />, name: "Trello", category: "Tools" },
-  { icon: <img src="/Postman.svg" className="w-10 h-10" />, name: "Postman", category: "Tools" },
+  { icon: <img src="/Swagger.svg" className="w-10 h-10" />, name: "Swagger", category: "Tools" },
+  { icon: <SiJira className="w-10 h-10 text-blue-400" />, name: "Jira", category: "Tools" },
+  { icon: <SiTrello className="w-10 h-10 text-sky-400" />, name: "Trello", category: "Tools" },
+  { icon: <img src="/Figma.svg" className="w-10 h-10" />, name: "Figma", category: "Tools" },
   { icon: <img src="/VisualStudio.svg" className="w-10 h-10" />, name: "Visual Studio", category: "Tools" },
   { icon: <img src="/code.svg" className="w-10 h-10" />, name: "VS Code", category: "Tools" },
 ];
@@ -86,13 +105,13 @@ const projects = [
       "Agile teamwork using Trello (Kanban)",
     ],
     technologies: [
-      "Node.js", "Express.js", "Sequelize", "PostgreSQL",
-      "Angular", "Ionic", "AWS", "Git", "API REST",
+      "Node.js", "Express.js", "Sequelize", "PostgreSQL","DBeaver",
+      "Angular", "Ionic", "AWS", "Github", "Swagger","Trello","Postman","VS Code"
     ],
     impact:
       "Delivered a secure and scalable auction mechanism with bid transparency and AWS-based deployment.",
-    link: "https://github.com/AnderSegovia/reverse-auction", // cambia si tienes repo privado
-    image: "/projects/reverse-auction.png",
+    link: "https://stirring-raindrop-a222c3.netlify.app/", 
+    images: ["/projects/13.png",]
   },
   {
     name: "University Social Service System",
@@ -107,13 +126,18 @@ const projects = [
       "PDF & Excel reporting integrations",
     ],
     technologies: [
-      "PHP", "Yii2", "Bootstrap", "JavaScript",
-      "MySQL", "Git", "API REST", "PDF/Excel Libraries",
+      "PHP", "Yii2","Composer", "HTML5","CSS3","Bootstrap", "JavaScript",
+      "MySQL", "Github", "Ubuntu", "Code",
     ],
     impact:
       "Launched July 2024, managing 1,000+ students & 500+ projects annually, optimizing administration.",
-    link: "https://github.com/AnderSegovia/social-service",
-    image: "/projects/social-service.png",
+    link: "https://github.com/Anderson-Castro72/ServicioSocialUNIVO.git",
+    images: ["/projects/0.png",
+      "/projects/1.png",
+      "/projects/2.png",
+      "/projects/3.png",
+      "/projects/4.png",
+    ]
   },
   {
     name: "Motorcycle Workshop Management",
@@ -129,12 +153,20 @@ const projects = [
     ],
     technologies: [
       "React", "Vite", "Node.js", "Express.js",
-      "MongoDB Atlas", "Axios", "Postman",
+      "MongoDB", "Postman","Code",
     ],
     impact:
       "Automated workflows, reduced inventory errors, and improved customer service for the business.",
     link: "https://visionary-jalebi-4dad95.netlify.app/",
-    image: "/projects/mototaller.png",
+      images: ["/projects/5.png",
+      "/projects/6.png",
+      "/projects/7.png",
+      "/projects/8.png",
+      "/projects/9.png",
+      "/projects/10.png",
+      "/projects/11.png",
+      "/projects/12.png",
+    ]
   },
 ];
 
@@ -178,63 +210,75 @@ const projects = [
       </section>
 
       {/* Tecnologías */}
-    <section className="max-w-6xl mx-auto py-16 px-4">
-      <h2 className="text-3xl font-semibold text-center mb-10">Tech Skills</h2>
+      <section className="max-w-6xl mx-auto py-16 px-4">
+        <h2 className="text-3xl font-semibold text-center mb-10">Tech Skills</h2>
 
-      {/* Botones de categorías */}
-      <div className="flex flex-wrap justify-center mb-8 gap-4">
-        {categories.map(cat => (
-          <button
-            key={cat}
-            className={`px-4 py-2 rounded-xl font-medium transition-colors ${
-              selectedCategory === cat ? "bg-blue-600 text-white" : "bg-gray-200 text-gray-800"
-            }`}
-            onClick={() => setSelectedCategory(cat)}
-          >
-            {cat}
-          </button>
-        ))}
-      </div>
+        {/* Botones de categorías */}
+        <div className="flex flex-wrap justify-center mb-8 gap-4">
+          {categories.map(cat => (
+            <button
+              key={cat}
+              className={`px-4 py-2 rounded-xl font-medium transition-colors ${
+                selectedCategory === cat ? "bg-blue-600 text-white" : "bg-gray-200 text-gray-800"
+              }`}
+              onClick={() => setSelectedCategory(cat)}
+            >
+              {cat}
+            </button>
+          ))}
+        </div>
 
-      {/* Tech Grid */}
-      {selectedCategory === "All" ? (
-        <div className="relative overflow-hidden w-full py-4">
-          <div className="animate-marquee-once">
-            {techs.map((t, i) => (
-              <div
-                key={i}
-                className="flex flex-col items-center p-4 bg-white rounded-2xl shadow transform transition duration-300 hover:scale-110 hover:rotate-6 hover:shadow-2xl min-w-[100px]"
-              >
-                <div className="text-5xl animate-spin-slow hover:animate-none">{t.icon}</div>
-                <p className="text-sm text-gray-700 text-center">{t.name}</p>
-              </div>
-            ))}
+        {/* Tech Grid */}
+        {selectedCategory === "All" ? (
+          <div className="relative overflow-hidden w-full py-4">
+            <div className="animate-marquee-once">
+              {techs.map((t, i) => (
+                <div
+                  key={i}
+                  className="flex flex-col items-center p-4 bg-white rounded-2xl shadow transform transition duration-300 hover:scale-110 hover:rotate-6 hover:shadow-2xl min-w-[100px]"
+                >
+                  <div className="text-5xl animate-spin-slow hover:animate-none">{t.icon}</div>
+                  <p className="text-sm text-gray-700 text-center">{t.name}</p>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-      ) : (
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6">
-          {techs
-            .filter(t => t.category === selectedCategory)
-            .map((t, i) => (
-              <div
-                key={i}
-                className="flex flex-col items-center p-4 bg-white rounded-2xl shadow transform transition duration-300 hover:scale-110 hover:rotate-6 hover:shadow-2xl"
-              >
-                <div className="text-5xl animate-spin-slow hover:animate-none">{t.icon}</div>
-                <p className="text-sm text-gray-700 text-center">{t.name}</p>
-              </div>
-            ))}
-        </div>
-      )}
+        ) : (
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6">
+            {techs
+              .filter(t => t.category === selectedCategory)
+              .map((t, i) => (
+                <div
+                  key={i}
+                  className="flex flex-col items-center p-4 bg-white rounded-2xl shadow transform transition duration-300 hover:scale-110 hover:rotate-6 hover:shadow-2xl"
+                >
+                  <div className="text-5xl animate-spin-slow hover:animate-none">{t.icon}</div>
+                  <p className="text-sm text-gray-700 text-center">{t.name}</p>
+                </div>
+              ))}
+          </div>
+        )}
 
-        </section>
+      </section>
 
       {/* Projects */}
       <section className="max-w-6xl mx-auto py-20 px-4">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {projects.map((project, i) => (
             <div key={i} className="p-6 bg-white rounded-2xl shadow hover:scale-105 transition">
-              <img src={project.image} alt={project.name} className="rounded-lg mb-4" />
+              {/* Mapeamos todas las imágenes */}
+              <div className="flex flex-col gap-2 mb-4">
+            <img
+              src={project.images[0]}
+              alt={project.name}
+              className="rounded-lg mb-4 cursor-pointer"
+              onClick={() => {
+                setActiveProject(project); 
+                setIndex(0); 
+              }}
+            />
+              </div>
+
               <h3 className="text-xl font-bold mb-1">{project.name}</h3>
               <p className="text-sm text-gray-500 mb-2">{project.period}</p>
               <p className="text-gray-700 mb-4">{project.description}</p>
@@ -256,7 +300,22 @@ const projects = [
             </div>
           ))}
         </div>
+
+        {/* Lightbox */}
+      {activeProject && (
+        <Lightbox
+          slides={activeProject.images.map((img) => ({ src: img }))}
+          open={index >= 0}
+          index={index}
+          close={() => {
+            setIndex(-1);
+            setActiveProject(null); 
+          }}
+        />
+      )}
       </section>
+
+      
 
       {/* Contact */}
       <section id="contact" className="max-w-3xl mx-auto py-20 px-4 text-center">
